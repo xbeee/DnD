b394edf6c50a785d092128b2e2b60e0446f493ee
Roltan <KhalikoR@yandex.ru>
2024-05-10 14:48:27 +0500

    авторизация

--- auth.py
+++ auth.py
@@ -4,32 +4,15 @@ from core import *
 # автаризация
 @api.route('/login', methods=['POST'])
 def Login():
-    # беру имя из формы
-    try:
-        name = request.form.get('name')
-    except:
-        resp = {
-            "errCode": 1,
-            "errString": "нехватает данных"
-        }
-        return resp, 401
-
-    # если не нашёл такого игрока
+    name = request.form.get('name')
     user = User.query.filter_by(name=name).first()
     if user is None:
-        resp = {
-            "errCode": 2,
-            "errString": "неверный логин"
-        }
-        return resp, 401
+        return redirect('/')
 
-    # меняю статус
-    user.status = True
-    db.session.commit()
-
-    token = create_access_token(identity=name)
+    if(user.role == 'GM'):
+        return render_template('gamemaster.html')
     heroes = []
-    if(user.role == 'user'):
+    if(user.role == 'User'):
         # если игрок
         query = Heroes.query.filter_by(user=name).all()
         for hero in query:
@@ -39,27 +22,18 @@ def Login():
                 'race': hero.rase,
                 'lvl': hero.lvl,
             })
-    return {
-        'access_token':token,
-        'role': user.role,
-        'heroes': heroes
-    }
+        return render_template('heroes.html', heroes=heroes)
 
 # выход из акаунта
-@api.route('/logout')
-@jwt_required()
-def Logout():
+@api.route('/logout/<name>')
+def Logout(name):
     # отключил игрока от лобби
-    name = get_jwt()["sub"]
     user = User.query.filter_by(name=name).first()
     user.status = False
     db.session.commit()
-    # убрал JWT
-    resp = jsonify({"msg":"logout successful"})
-    unset_jwt_cookies(resp)
     # уведомление мастеру
     send('disconnect', {
         'msg': 'disconnect',
         'user': name
     })
-    return resp
+    return redirect('/')
--- main.py
+++ main.py
@@ -4,5 +4,9 @@ import auth
 with api.app_context():
     db.create_all()
 
+@api.route('/')
+def Index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     socketio.run(api)
